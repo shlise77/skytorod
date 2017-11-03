e8b00db96fe93376685ceed697709205926bc5e3
user <user@DESKTOP-23JS0J1>
2017-11-03 16:14:38 +0900

    123

--- test2/src/main/java/com/test/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.test.service;
-
-import com.test.DTO.LoginDTO;
-import com.test.domain.LoginVO;
-
-public interface LoginService{
-	public LoginVO login(LoginDTO dto)throws Exception;
-
-}
